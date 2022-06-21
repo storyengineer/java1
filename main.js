42e26b23ea277fa0beb83f2740090604eda7d953
Eke Kalu <ekendukwe@gmail.com>
2022-06-21 12:03:28 -0300

    added code

---
+++ main.js
@@ -0,0 +1,10 @@
+name1 = "Eke Ndukwe Kalu";
+height1ininches = 72;
+country = "Nigeria";
+
+console.log("My name is: ", name1);
+console.log("My height in inches is: ", height1ininches);
+console.log("I am from: ", country);
+
+
+
